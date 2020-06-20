b35c92d453dde39bc7445a23da19a05fd96995bd
DongheeHam <kshham@naver.com>
2020-06-20 22:02:30 +0900

    작업중

--- TDCNS_admin/docs/TDCNS_admin.sql
+++ TDCNS_admin/docs/TDCNS_admin.sql
@@ -6,7 +6,8 @@ CREATE TABLE INTERSECTIONS(
 	NAME 		varchar(256) COMMENT "교차로 이름",
 	POS 		POINT 	COMMENT "교차로위치",
 	
-	primary key (INO)
+	primary key (INO),
+	index INTER_NAME (NAME)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS ROAD;
@@ -14,15 +15,14 @@ CREATE TABLE ROAD(
 	RNO 		int(11) not null COMMENT "진입로 고유번호",
 	INO 		int(11) not null COMMENT "진입로가 소속된 교차로 고유번호",
 	NAME 		varchar(256) COMMENT "진입로 이름",
-	DTC 		varchar(3000) COMMENT "진입로에서 카메라가 인식할 범위",
-	LDTC 		varchar(3000) COMMENT "진입로에서 카메라가 인식할 범위",
 	STREAM 		varchar(512) COMMENT "진입로 카메라의 stream url",
 	PW 			varchar(512) COMMENT "stream url의 접속 비밀번호 (예상)",
 	CNO 		varchar(64) COMMENT "카메라 고유번호",
 	
 	primary key (RNO),
 	index ROAD_INO (INO),
-	index ROAD_CAM (CNO)
+	index ROAD_CAM (CNO),
+	index ROAD_NAME (NAME)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS CAM;
@@ -49,8 +49,8 @@ CREATE TABLE LDTC(
 	RNO 		int(11) not null COMMENT "ROAD 고유번호",
 	LANE 		int(11) not null COMMENT "차선(1~n)",
 	POINT 		int(11) not null COMMENT "몇번째 점인지 0~n",
-	DX 		int(11) COMMENT "x좌표",
-	DY 			int(11) COMMENT "y좌표",
+	LX 		int(11) COMMENT "x좌표",
+	LY 			int(11) COMMENT "y좌표",
 	
 	primary key (RNO, LANE, POINT)
 ) ENGINE=InnoDB;
